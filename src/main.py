a15a623b1c2bc5c375417cb850bd5ce4c7499b3b
Ruslan Alimov <ruslan.alimov@umbrellait.com>
2022-01-26 18:49:06 +0300

    Delete comments

--- src/main.py
+++ src/main.py
@@ -1,31 +1,20 @@
-import logging
-
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 from core import config
-from core.logger import LOGGING
+
 
 app = FastAPI(
-    # Конфигурируем название проекта. Оно будет отображаться в документации
     title=config.PROJECT_NAME,
-    # Адрес документации в красивом интерфейсе
     docs_url='/api/openapi',
-    # Адрес документации в формате OpenAPI
     openapi_url='/api/openapi.json',
-    # Можно сразу сделать небольшую оптимизацию сервиса
-        # и заменить стандартный JSON-сереализатор на более шуструю версию, написанную на Rust
     default_response_class=ORJSONResponse,
 )
 
 if __name__ == '__main__':
-    # Приложение может запускаться командой
-    # `uvicorn main:app --host 0.0.0.0 --port 8000`
-    # но чтобы не терять возможность использовать дебагер,
-    # запустим uvicorn сервер через python
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
         port=8000,
-    ) 
+    )

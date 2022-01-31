9c705e66d0e9546ddf9bfc70e1fc82040aa5b292
Ivan <ivanbrut@gmail.com>
2022-02-01 00:15:25 +0800

    Made models for film, genre and person

--- src/models/genre.py
+++ src/models/genre.py
@@ -4,36 +4,28 @@ from pydantic import BaseModel
 def orjson_dumps(v, *, default):
     return orjson.dumps(v, default=default).decode()
 
-class Film(BaseModel):
-    __slots__ = ['id', 'imdb_rating', 'title', 'description', 'directors_names', 'actors_names', 'writers_names', 'genres_names' ]
+class Genre(BaseModel):
+    __slots__ = ['id', 'name', 'description']
     id: str
-    imdb_rating: float
-    title: str
-    description: str
-    directors_names: set
-    actors_names: set
-    writers_names: set
-    genres_names: set
-
+    name: str
+    description: None
 
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
 
 
-def create_film_object(search_result: dict) -> Film:
-
     '''
     :param search_result: функция получает данные поска Elastic Search в виде словаря
     :return: Возвращается объект FastApi с выставленными данными.
     '''
 
+
+def create_genres_list(search_result: dict) -> Genre:
     reorganize_es_data = {}
-    for slot in Film.__slots__:
+    for slot in Genre.__slots__:
         reorganize_es_data[slot] = search_result['_source'][slot]
 
-    detailed_film_object = Film(**reorganize_es_data)
-    return detailed_film_object
 
-def create_film_list(search_result: dict):
-    pass
+    detailed_genre_object = Genre(**reorganize_es_data)
+    return detailed_genre_object
--- src/models/person.py
+++ src/models/person.py
@@ -4,36 +4,28 @@ from pydantic import BaseModel
 def orjson_dumps(v, *, default):
     return orjson.dumps(v, default=default).decode()
 
-class Film(BaseModel):
-    __slots__ = ['id', 'imdb_rating', 'title', 'description', 'directors_names', 'actors_names', 'writers_names', 'genres_names' ]
+class Person(BaseModel):
+    __slots__ = ['id', 'name', 'birth_date']
     id: str
-    imdb_rating: float
-    title: str
-    description: str
-    directors_names: set
-    actors_names: set
-    writers_names: set
-    genres_names: set
-
+    name: str
+    birth_date: None
 
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
 
 
-def create_film_object(search_result: dict) -> Film:
-
     '''
     :param search_result: функция получает данные поска Elastic Search в виде словаря
     :return: Возвращается объект FastApi с выставленными данными.
     '''
 
+
+def create_person_object(search_result: dict) -> Person:
     reorganize_es_data = {}
-    for slot in Film.__slots__:
+    for slot in Person.__slots__:
         reorganize_es_data[slot] = search_result['_source'][slot]
 
-    detailed_film_object = Film(**reorganize_es_data)
-    return detailed_film_object
 
-def create_film_list(search_result: dict):
-    pass
+    detailed_person_object = Person(**reorganize_es_data)
+    return detailed_person_object

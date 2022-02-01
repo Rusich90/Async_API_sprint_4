c36dcaec80e63e30be4c8947a2a073cc34946402
Ivan <ivanbrut@gmail.com>
2022-02-01 12:34:42 +0800

    Created film, genre & person models.
    According to services requirements.

--- src/models/genre.py
+++ src/models/genre.py
@@ -1,31 +1,15 @@
 import orjson
 from pydantic import BaseModel
+from typing import Optional
 
 def orjson_dumps(v, *, default):
     return orjson.dumps(v, default=default).decode()
 
 class Genre(BaseModel):
-    __slots__ = ['id', 'name', 'description']
     id: str
     name: str
-    description: None
+    description: Optional[None] = str
 
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
-
-
-    '''
-    :param search_result: функция получает данные поска Elastic Search в виде словаря
-    :return: Возвращается объект FastApi с выставленными данными.
-    '''
-
-
-def create_genres_list(search_result: dict) -> Genre:
-    reorganize_es_data = {}
-    for slot in Genre.__slots__:
-        reorganize_es_data[slot] = search_result['_source'][slot]
-
-
-    detailed_genre_object = Genre(**reorganize_es_data)
-    return detailed_genre_object
--- src/models/person.py
+++ src/models/person.py
@@ -1,31 +1,16 @@
 import orjson
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
 def orjson_dumps(v, *, default):
     return orjson.dumps(v, default=default).decode()
 
 class Person(BaseModel):
-    __slots__ = ['id', 'name', 'birth_date']
     id: str
     name: str
-    birth_date: None
+    birth_date: Optional[None] = datetime
 
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
-
-
-    '''
-    :param search_result: функция получает данные поска Elastic Search в виде словаря
-    :return: Возвращается объект FastApi с выставленными данными.
-    '''
-
-
-def create_person_object(search_result: dict) -> Person:
-    reorganize_es_data = {}
-    for slot in Person.__slots__:
-        reorganize_es_data[slot] = search_result['_source'][slot]
-
-
-    detailed_person_object = Person(**reorganize_es_data)
-    return detailed_person_object

951d15588b3b70bce1fb86f938c9f8f572da9064
rusich90 <rusichviz@gmail.com>
2022-02-03 21:10:56 +0300

    Added dotenv library

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,13 @@
 import os
 from logging import config as logging_config
 
+from dotenv import load_dotenv
+
 from core.logger import LOGGING
+
+
+load_dotenv()
+
 
 logging_config.dictConfig(LOGGING)
 

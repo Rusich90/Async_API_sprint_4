1d1d8c23829e16b23ea8a9e3679fc6153f748160
Ivan <ivanbrut@gmail.com>
2022-02-07 17:18:56 +0800

    Apdate Film logic. Make find by keyword

---
+++ src/models/film.py
@@ -0,0 +1,28 @@
+import orjson
+from pydantic import BaseModel
+from typing import List
+
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
+
+class NameId(BaseModel):
+    id: str
+    name: str
+
+class Film(BaseModel):
+    id: str
+    imdb_rating: float
+    title: str
+    description: str
+    genres_names: list
+    directors_names: list
+    actors_names: list
+    writers_names: list
+    actors: List[NameId]
+    directors: List[NameId]
+    writers: List[NameId]
+    genres: List[NameId]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
---
+++ src/models/keyword.py
@@ -0,0 +1,31 @@
+import orjson
+from pydantic import BaseModel
+from typing import List
+
+from src.models.film import Film
+
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
+
+class PageData(BaseModel):
+    scroll_page: int
+    results_ids: List[str]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+class Keyword(BaseModel):
+    scroll_page: int
+    total_pages: int
+    results: List[Film]
+
+class KeywordCash(BaseModel):
+    tokens_hash: str
+    total_pages: int
+    scroll_id: str
+    pages: dict
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps

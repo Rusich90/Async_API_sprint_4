f30d512aba052bae41a0196522b5e45297764eb2
Ivan <ivanbrut@gmail.com>
2022-02-07 17:24:27 +0800

    Make cash information process, in a bundle witp pagination
    To paginate add ?page=<page>

---
+++ src/services/film.py
@@ -0,0 +1,64 @@
+from functools import lru_cache
+
+from aioredis import Redis
+from elasticsearch import AsyncElasticsearch
+from fastapi import Depends
+from typing import Optional
+
+from db.elastic import get_elastic
+from db.redis import get_redis
+from models.film import Film
+
+FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5  # 5 минут
+
+class FilmService:
+    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
+        self.redis = redis
+        self.elastic = elastic
+
+    async def get_by_id(self, film_id: str) -> Optional[Film]:
+        film = await self._film_from_cache(film_id)
+        if not film:
+            film = await self._get_film_from_elastic(film_id)
+            # print(film)
+            if not film:
+                # Если он отсутствует в Elasticsearch, значит, фильма вообще нет в базе
+                return None
+            # Сохраняем фильм  в кеш
+            await self._put_film_to_cache(film)
+        return film
+
+    async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
+        doc = await self.elastic.get('movies', film_id)
+
+        print(doc)
+        print(type(doc))
+
+        return Film(**doc['_source'])
+
+    async def _film_from_cache(self, film_id: str) -> Optional[Film]:
+        data = await self.redis.get(film_id)
+        if not data:
+            return None
+
+        # При отладке моделей возможо кэширование неполной модели.
+        # В случае несоотвествия полей, расцениваем как отсутсвие записи
+        try:
+            film = Film.parse_raw(data)
+        except:
+            return None
+        return film
+
+    async def _put_film_to_cache(self, film: Film):
+        # Сохраняем данные о фильме, используя команду set
+        # Выставляем время жизни кеша — 5 минут
+        # https://redis.io/commands/set
+        # pydantic позволяет сериализовать модель в json
+        await self.redis.set(film.id, film.json(), expire=FILM_CACHE_EXPIRE_IN_SECONDS)
+
+
+@lru_cache()
+def get_film_service(
+        redis: Redis = Depends(get_redis),
+        elastic: AsyncElasticsearch = Depends(get_elastic),) -> FilmService:
+    return FilmService(redis, elastic)
